437e4d3c673821f52c361c83317a680bf5be9576
k.yarilin <tooler53@outlook.com>
2019-04-30 19:09:12 +0300

    Включил автозагрузку в проект

--- index.php
+++ index.php
@@ -13,12 +13,10 @@ error_reporting(E_ALL);
 //2. Подключение файлов системы
 
 define('ROOT', dirname(__FILE__));
-require_once(ROOT . '/components/Router.php');
+require_once(ROOT . '/vendor/autoload.php');
 //3. Установка соединения с БД
 
 //4. Вызов Router
 
 $router = new Router();
 $router->run();
-
-echo 'index';
